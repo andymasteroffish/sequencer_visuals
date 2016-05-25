ecabe178fe71d6699e8848da6740e1e4b5837f64
Andy Wallace <andymasteroffish@gmail.com>
2016-05-25 16:52:42 -0400

    square hit

---
+++ sequencer/src/SquareHit.cpp
@@ -0,0 +1,9 @@
+//
+//  SquareHit.cpp
+//  sequencer
+//
+//  Created by Andrew Wallace on 5/25/16.
+//
+//
+
+#include "SquareHit.hpp"
---
+++ sequencer/src/SquareHit.hpp
@@ -0,0 +1,14 @@
+//
+//  SquareHit.hpp
+//  sequencer
+//
+//  Created by Andrew Wallace on 5/25/16.
+//
+//
+
+#ifndef SquareHit_hpp
+#define SquareHit_hpp
+
+#include <stdio.h>
+
+#endif /* SquareHit_hpp */
