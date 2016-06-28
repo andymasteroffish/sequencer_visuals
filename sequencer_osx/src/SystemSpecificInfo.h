e0607142ee680c1966e048ce61b26233ec36488f
Andy Wallace <andymasteroffish@gmail.com>
2016-06-28 17:43:45 -0400

    making sure all is still cool on osx

---
+++ sequencer_osx/src/SystemSpecificInfo.h
@@ -0,0 +1,15 @@
+//
+//  SystemSpecificInfo.h
+//  sequencer
+//
+//  Created by Andrew Wallace on 6/28/16.
+//
+//
+
+#ifndef SystemSpecificInfo_h
+#define SystemSpecificInfo_h
+
+#define USING_OSX
+
+
+#endif /* SystemSpecificInfo_h */
