f4e0a7581db7064a4f39bf3c5a1334a1bd02e463
Andy Wallace <andymasteroffish@gmail.com>
2017-06-08 14:20:32 -0400

    windows project compiles but sound crappy when sounds overlap

---
+++ sequencer_win/src/ofApp.cpp
@@ -0,0 +1,68 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	sequencer.setup();
+
+	//for maxim
+	ofSoundStreamSetup(2, 0, this, sequencer.sampleRate, sequencer.bufferSize, 4);
+
+}
+
+
+//--------------------------------------------------------------
+void ofApp::update() {
+	//cout<<"--FRAME "<<ofGetElapsedTimeMillis()<<" --"<<endl;
+
+	sequencer.update();
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	sequencer.draw();
+
+	if (sequencer.takeScreenshot) {
+		sequencer.takeScreenshot = false;
+		ofImage pic;
+		pic.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+		pic.save("screenshots/img" + ofToString(ofGetSystemTime()) + ".png");
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+	sequencer.touchDown(x, y);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	sequencer.keyPressed(key);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+	sequencer.mouseMoved(x, y);
+}
+
+
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+	sequencer.windowResized(w, h);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::audioOut(float * output, int bufferSize, int nChannels) {
+	for (int i = 0; i < bufferSize; i++) {
+		sequencer.setMaximAudio(i % 2 == 0);
+		output[i*nChannels] = sequencer.sampleOut;
+		output[i*nChannels + 1] = sequencer.sampleOut;
+	}
+}
+
---
+++ sequencer_win/src/ofApp.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "ofMain.h"
+
+#include "Sequencer.hpp"
+
+class ofApp : public ofBaseApp {
+
+public:
+
+	void setup();
+	void update();
+	void draw();
+
+	void mousePressed(int x, int y, int button);
+	void mouseMoved(int x, int y);
+
+	void keyPressed(int key);
+	void windowResized(int w, int h);
+
+	void audioOut(float * output, int bufferSize, int nChannels);
+
+	Sequencer sequencer;
+
+
+	//    void keyReleased(int key);
+	//    void mouseMoved(int x, int y );
+	//    void mouseDragged(int x, int y, int button);
+	//    void mousePressed(int x, int y, int button);
+	//    void mouseReleased(int x, int y, int button);
+	//    void mouseEntered(int x, int y);
+	//    void mouseExited(int x, int y);
+	//    void dragEvent(ofDragInfo dragInfo);
+	//    void gotMessage(ofMessage msg);
+
+
+};
